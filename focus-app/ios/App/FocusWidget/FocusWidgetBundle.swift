d7717166d3dc2810dbc2b4e55fd455fb011e95a3
Mateus Gomes <mateusgomesbarros@hotmail.com>
2026-08-04 21:07:30 -0300

    feat: add iOS widget extension and app icon

---
+++ focus-app/ios/App/FocusWidget/FocusWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  FocusWidgetBundle.swift
+//  FocusWidget
+//
+//  Created by Mateus Gomes on 04/08/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FocusWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FocusWidget()
+        FocusWidgetControl()
+    }
+}
