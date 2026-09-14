dffdb744c4e4d306849e7c4dcaa7141a7e9e7ec6
Mateus Gomes barros <90994257+mateus-gomes-barros@users.noreply.github.com>
2026-09-14 05:06:32 -0300

    fix(wear): correct graphicsLayer import

--- focus-app/android/wear/src/main/java/com/mateusgomes/focusapp/pulse/MainActivity.kt
+++ focus-app/android/wear/src/main/java/com/mateusgomes/focusapp/pulse/MainActivity.kt
@@ -34,7 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.graphicsLayer
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
