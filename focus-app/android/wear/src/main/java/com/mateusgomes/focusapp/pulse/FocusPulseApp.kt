e07ae583e4c81cd6661103c5b81d67150aba71c0
Mateus Gomes barros <90994257+mateus-gomes-barros@users.noreply.github.com>
2026-09-17 00:06:59 -0300

    fix(wear): import rotary scroll extension

--- focus-app/android/wear/src/main/java/com/mateusgomes/focusapp/pulse/FocusPulseApp.kt
+++ focus-app/android/wear/src/main/java/com/mateusgomes/focusapp/pulse/FocusPulseApp.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
